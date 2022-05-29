0908f60d3a6347ed25c34037d43edfa2b7b374bb
Shinichi Hashitani <hashitani@icloud.com>
2022-05-29 11:55:56 +0900

    Labs seting update - changing LM SMT configuration

--- labs/connect-scripts/startKafkaConnectComponents.sh
+++ labs/connect-scripts/startKafkaConnectComponents.sh
@@ -8,10 +8,11 @@ echo "Launching Kafka Connect worker"
 /etc/confluent/docker/run &
 #
 echo "waiting 2 minutes for things to stabilise"
-sleep 120
+sleep 60
 echo "Starting HTTP Sink Connector"
 
 HEADER="Content-Type: application/json"
+
 DATA=$(
   cat <<EOF
 {
@@ -20,26 +21,20 @@ DATA=$(
     "topics": "http-messages",
     "tasks.max": "1",
     "connector.class": "io.confluent.connect.http.HttpSinkConnector",
-    "http.api.url": "http://rest-proxy:8082/topics/test-http",
+    "http.api.url": "http://rest-proxy:8082/topics/rest-result",
     "value.converter": "org.apache.kafka.connect.storage.StringConverter",
     "confluent.topic.bootstrap.servers": "broker:29092",
     "confluent.topic.replication.factor": "1",
+    "headers": "Content-Type: application/vnd.kafka.json.v2+json",
     "reporter.bootstrap.servers": "broker:29092",
     "reporter.result.topic.name": "success-responses",
     "reporter.result.topic.replication.factor": "1",
     "reporter.error.topic.name":"error-responses",
     "reporter.error.topic.replication.factor":"1",
-    "transforms": "insertAppIdHeader1",
-    "transforms": "insertAppIdHeader1.type": "org.apache.kafka.connect.transforms.InsertHeader",
-    "transforms.insertAppIdHeader1.header": "Content-Type",
-    "transforms.insertAppIdHeader1.value.literal": "application/vnd.kafka.v2+json",
-    "transforms": "insertAppIdHeader2",
-    "transforms": "insertAppIdHeader2.type": "org.apache.kafka.connect.transforms.InsertHeader",
-    "transforms.insertAppIdHeader2.header": "Accept",
-    "transforms.insertAppIdHeader2.value.literal": "application/vnd.kafka.v2+json"
-    "transform": "InsertLMv1Token",
-    "transform.InsertLMv1Token.ACCESS_ID": "testaccessid123",
-    "transform.InsertLMv1Token.ACCESS_KEY": "testaccesskey123"
+    "transforms": "InsertLMv1Token",
+    "transforms.InsertLMv1Token.type": "work.hashi.kafka.smtLabs.InsertLMv1Token",
+    "transforms.InsertLMv1Token.access.id": "testaccessid123",
+    "transforms.InsertLMv1Token.access.key": "testaccesskey123"
   }
 }
 EOF

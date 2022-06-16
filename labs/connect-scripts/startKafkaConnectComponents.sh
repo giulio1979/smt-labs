207afdc1915e55a2244e4cdd6134426b8abfc61d
Shinichi Hashitani <hashitani@icloud.com>
2022-06-16 17:36:58 +0900

    Included logic to add device id in InsertLMv1Token.

--- labs/connect-scripts/startKafkaConnectComponents.sh
+++ labs/connect-scripts/startKafkaConnectComponents.sh
@@ -3,7 +3,7 @@ echo "Installing connector plugins"
 confluent-hub install --no-prompt confluentinc/kafka-connect-http:1.5.3
 confluent-hub install --no-prompt confluentinc/connect-transforms:1.4.3
 echo "Installing SMTs from volume"
-cp /tmp/smt/*.jar /usr/share/confluent-hub-components
+cp /tmp/smt/*dependencies.jar /usr/share/confluent-hub-components
 #
 echo "Launching Kafka Connect worker"
 /etc/confluent/docker/run &
@@ -22,7 +22,7 @@ DATA=$(
     "topics": "http-messages",
     "tasks.max": "1",
     "connector.class": "io.confluent.connect.http.HttpSinkConnector",
-    "http.api.url": "http://rest-proxy:8082/topics/rest-result",
+    "http.api.url": "https://webhook.site/xxxx",
     "key.converter": "org.apache.kafka.connect.storage.StringConverter",
     "value.converter": "org.apache.kafka.connect.json.JsonConverter",
     "value.converter.schemas.enable": "false",
@@ -33,15 +33,13 @@ DATA=$(
     "batch.json.as.array": "true",
     "reporter.result.topic.name": "",
     "reporter.error.topic.name": "",
-    "transforms": "HoistField,InsertField,InsertLMv1Token",
-    "transforms.HoistField.type": "org.apache.kafka.connect.transforms.HoistField$Value",
+    "transforms": "HoistField,InsertLMv1Token",
+    "transforms.HoistField.type": "org.apache.kafka.connect.transforms.HoistField\$Value",
     "transforms.HoistField.field": "msg",
-    "transforms.InsertField.type": "org.apache.kafka.connect.transforms.InsertField$Value",
-    "transforms.InsertField.static.field": "_lm.resourceid",
-    "transforms.InsertField.static.value": "{\"system.deviceid\": \"41\"}",
     "transforms.InsertLMv1Token.type": "work.hashi.kafka.smtLabs.InsertLMv1Token",
     "transforms.InsertLMv1Token.access.id": "testaccessid123",
-    "transforms.InsertLMv1Token.access.key": "testaccesskey123"
+    "transforms.InsertLMv1Token.access.key": "testaccesskey123",
+    "transforms.InsertLMv1Token.device.id": "123"
   }
 }
 EOF
--- src/main/java/work/hashi/kafka/smtLabs/InsertLMv1Token.java
+++ src/main/java/work/hashi/kafka/smtLabs/InsertLMv1Token.java
@@ -1,11 +1,13 @@
 package work.hashi.kafka.smtLabs;
 
+import java.util.HashMap;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.ConnectRecord;
 import org.apache.kafka.connect.data.Values;
 import org.apache.kafka.connect.header.Headers;
 import org.apache.kafka.connect.transforms.Transformation;
 import org.apache.kafka.connect.transforms.util.SimpleConfig;
+import org.apache.kafka.connect.transforms.util.Requirements;
 
 import com.logicmonitor.auth.LMv1TokenGenerator;
 
@@ -19,6 +21,9 @@ public class InsertLMv1Token<R extends ConnectRecord<R>> implements Transformati
     public static final String OVERVIEW_DOC = "Add LM v1 Token into header for authorization.";
     public static final String ACCESS_KEY = "access.key";
     public static final String ACCESS_ID = "access.id";
+    public static final String DEVICE_ID = "device.id";
+    public static final String RESOURCE_ID_MESSAGE_KEY = "_lm.resourceid";
+    public static final String DEVICE_ID_MESSAGE_KEY = "system.deviceid";
     public static final String AUTH_HEADER_FIELD = "Authorization";
     public static final String HTTP_VERB = "POST";
     public static final String RESOURCE_PATH = "/log/ingest";
@@ -31,17 +36,27 @@ public class InsertLMv1Token<R extends ConnectRecord<R>> implements Transformati
             .define(ACCESS_ID, ConfigDef.Type.STRING,
                     NO_DEFAULT_VALUE, new ConfigDef.NonNullValidator(),
                     ConfigDef.Importance.HIGH,
-                    "Logic Monitor Access ID");
-
+                    "Logic Monitor Access ID")
+            .define(DEVICE_ID, ConfigDef.Type.STRING,
+                    NO_DEFAULT_VALUE, new ConfigDef.NonNullValidator(),
+                    ConfigDef.Importance.HIGH,
+                    "Logic Monitor Device ID");
     private String accessKey;
     private String accessId;
+    private String deviceId;
 
     @Override
     public R apply(R record) {
         Headers updatedHeaders = record.headers().duplicate();
         updatedHeaders.add(AUTH_HEADER_FIELD, Values.parseString(generateLMv1Token(record.value())));
+
+        Map<String, Object> resourceIdNode = new HashMap<String, Object>();
+        resourceIdNode.put(DEVICE_ID_MESSAGE_KEY, deviceId);
+        Map<String, Object> updatedValue = new HashMap<>(Requirements.requireMap(record.value(), ""));
+        updatedValue.put(RESOURCE_ID_MESSAGE_KEY, resourceIdNode);
+
         return record.newRecord(record.topic(), record.kafkaPartition(), record.keySchema(), record.key(),
-                record.valueSchema(), record.value(), record.timestamp(), updatedHeaders);
+                record.valueSchema(), updatedValue, record.timestamp(), updatedHeaders);
     }
 
     @Override
@@ -57,6 +72,7 @@ public class InsertLMv1Token<R extends ConnectRecord<R>> implements Transformati
         final SimpleConfig config = new SimpleConfig(CONFIG_DEF, props);
         accessKey = config.getString(ACCESS_KEY);
         accessId = config.getString(ACCESS_ID);
+        deviceId = config.getString(DEVICE_ID);
     }
 
     private String generateLMv1Token(Object fieldMessage) {
